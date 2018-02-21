a1c4cc466c973a1f20dd79baa6fe4ba7bceba21e
Jesús Giganto Diez <Jesus.Giganto@tajamar365.com>
2018-02-21 20:13:32 +0100

    a

---
+++ MVCPaginaryDetalle/Controllers/HospitalController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCPaginaryDetalle.Models;
+
+namespace MVCPaginaryDetalle.Controllers
+{
+    public class HospitalController : Controller
+    {
+        ModeloHospital modelo;
+        public HospitalController()
+        {
+            modelo = new ModeloHospital();
+        }
+        // GET: Hospital
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MaestroDetalle(int? posicion,String hospitalcod)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+            }
+            int ultimo = modelo.GetNumeroHospitales();
+            int siguiente = posicion.GetValueOrDefault() + 1;
+            if (siguiente > ultimo)
+            {
+                siguiente = ultimo;
+            }
+            int anterior = posicion.GetValueOrDefault() - 1;
+            if (anterior < 1) { anterior = 1; }
+            PAGINARHOSPITAL_Result hospital = modelo.GetHospitalPaginado(posicion.GetValueOrDefault());
+            List<DOCTOR> lista =
+                modelo.GetDoctoresHospital(hospital.HOSPITAL_COD);
+            ViewBag.Doctores = lista;
+            ViewBag.Ultimo = ultimo;
+            ViewBag.Siguiente = siguiente;
+            ViewBag.Anterior = anterior;
+
+            return View(hospital);
+
+
+
+
+        }
+    }
+}
---
+++ MVCPaginaryDetalle/Models/ModeloHospital.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+#region PROCEDIMIENTOS ALMACENADOS
+
+//CREATE PROCEDURE PAGINAR_HOSPITAL
+//(@POSICION INT) AS
+//SELECT* FROM
+//(SELECT ROW_NUMBER()
+//OVER (ORDER BY HOSPITAL_COD)
+//AS POSICION, NOMBRE, DIRECCION
+//, TELEFONO, NUM_CAMA, HOSPITAL_COD
+//FROM HOSPITAL) HOSPITALES
+//WHERE POSICION = @POSICION
+//GO
+
+#endregion
+
+namespace MVCPaginaryDetalle.Models
+{
+    public class ModeloHospital
+    {
+        ContextoHospital contexto;
+        public ModeloHospital()
+        {
+            contexto = new ContextoHospital();
+        }
+        public PAGINARHOSPITAL_Result GetHospitalPaginado (int posicion)
+        {
+            var resultado = contexto.PAGINARHOSPITAL(posicion);
+            return resultado.FirstOrDefault();
+        }
+        public int GetNumeroHospitales()
+        {
+            var consulta = (from datos in contexto.HOSPITAL
+                            select datos).Count();
+            return consulta;
+        }
+        public List<DOCTOR> GetDoctoresHospital(string hospitalcod)
+        {
+            var consulta = from datos in contexto.DOCTOR
+                           where datos.HOSPITAL_COD == hospitalcod
+                           select datos;
+
+            if (consulta.Count() == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return consulta.ToList();
+            }
+        }
+       
+            
+    }
+}
